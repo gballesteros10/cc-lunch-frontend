2d481b58bf4ebe0cebe8d9b7a9a95342711f578f
gballesteros <geraldballesteros@outlook.com>
2020-01-10 14:12:57 +0800

    init project. based from create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class App extends Component<{}, {}> {
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar bg-dark text-white">
+          <span className="navbar-brand mb-0 h1">CC Employee Lunch</span>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
