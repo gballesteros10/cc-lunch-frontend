0b9a5d21e86358fe6f91c87ae05513e2f801a5a0
gballesteros <geraldballesteros@outlook.com>
2020-01-12 23:23:05 +0800

    redirect to login page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,36 +3,47 @@ import React, { Component } from 'react';
 import EmployeeView from './employee/EmployeeView';
 import EmployerTableView from './employer/EmployerTableView';
 import Loader from './Loader';
-import { IsUserAdmin } from '../api/UserAPI';
+import { GetUser } from '../api/UserAPI';
+import { User } from '../lib/interfaces';
+import UserLogin from './user/UserLogin';
 
 interface IState {
-    isAdmin?: boolean;
+    user?: User;
+    isDoneFetching: boolean;
 }
 
 class Home extends Component<{}, IState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            isAdmin: undefined
+            user: undefined,
+            isDoneFetching: false
         }
     }
 
     componentWillMount() {
         // I know, ugly workaround
         // render gets executed first before localStorage.setItem
-        setTimeout(() => this.setState({ isAdmin: IsUserAdmin() }), 1000)
+        setTimeout(() => {
+            this.setState({
+                user: GetUser(),
+                isDoneFetching: true
+            })
+        }, 1000)
     }
 
     render() {
-        if (this.state.isAdmin === undefined) {
-            return <Loader />;
+        if (this.state.user) {
+            if (this.state.user.is_admin)
+                return <EmployerTableView />
+            return <EmployeeView />
         }
 
-        if (this.state.isAdmin) {
-            return <EmployerTableView />;
+        if (this.state.isDoneFetching) {
+            return <UserLogin />;
         }
 
-        return <EmployeeView />;
+        return <Loader />;
     }
 }
 
